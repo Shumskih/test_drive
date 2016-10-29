3e60d1be6c1c160fcf6256b26fb577d632c89aa0
Shumskih <music.dll@mail.ru>
2016-10-29 03:13:29 +0300

    Added mileage method with parameters and arguments

--- test_drive.rb
+++ test_drive.rb
@@ -3,17 +3,31 @@ def accelerate
   puts "Speeding up!"
 end
 
+
 def sound_horn
   puts "Pressing the horn button"
   puts "Beeb Beeb!"
 end
+
 
 def use_headlights(brightness = "low-beam")
   puts "Turning on #{brightness} headlights"
   puts "Watch out for deer"
 end
 
+
+def mileage(miles_driven, gas_used)
+  return miles_driven / gas_used
+end
+
+
 sound_horn
 accelerate
 use_headlights
 use_headlights("high-beam")
+
+trip_mileage = mileage(400, 12)
+puts "You got #{trip_mileage} MPG on this trip."
+
+lifetime_mileage = mileage(11432, 366)
+puts "This car average #{lifetime_mileage} MPG."
